b4543c2d5970248797a0e6597216eb24cf6e335c
Daniel Le Berre <daniel.leberre@univ-artois.fr>
2019-10-02 08:05:58 +0200

    :white_check_mark: rely only on jupyter classes

--- tst/fr/univartois/iliframework/EnumerableTest.java
+++ tst/fr/univartois/iliframework/EnumerableTest.java
@@ -1,6 +1,7 @@
 package fr.univartois.iliframework;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import java.util.Enumeration;
 import java.util.Vector;
