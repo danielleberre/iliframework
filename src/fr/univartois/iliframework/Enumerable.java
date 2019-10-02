358d51e9910b1163473338ed1465fcab2490cabe
Daniel Le Berre <daniel.leberre@univ-artois.fr>
2019-10-02 22:26:39 +0200

    :pencil: added javadoc

--- src/fr/univartois/iliframework/Enumerable.java
+++ src/fr/univartois/iliframework/Enumerable.java
@@ -3,6 +3,17 @@ package fr.univartois.iliframework;
 import java.util.Enumeration;
 import java.util.Iterator;
 
+/**
+ * Utility class to allow the use of for each loops
+ * with Enumeration objets.
+ * 
+ * It is a simple Object Adapter for the Iterable interface
+ * using type inference to simplify it's use by the end user.
+ * 
+ * @author leberre
+ *
+ * @param <T> the type of the elements to iterate on
+ */
 public class Enumerable<T> implements Iterable<T> {
 
 	private Enumeration<T> adapted;
@@ -27,6 +38,13 @@ public class Enumerable<T> implements Iterable<T> {
 		};
 	}
 	
+	/**
+	 * Factory method used to derive the type of the Enumerable from the Enumeration object.
+	 * 
+	 * @param <T> the type of the elements of adapted
+	 * @param adapted the Enumeration to iterate on in the for loop
+	 * @return an Enumerable object of the required type
+	 */
 	public static <T> Enumerable<T> of(Enumeration<T> adapted) {
 		return new Enumerable<>(adapted);
 	}
